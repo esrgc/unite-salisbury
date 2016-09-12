af450851c3a4628333ec14a85749aeac6388d67a
tahoang <dhoang519@gmail.com>
2016-09-12 10:53:22 -0400

    test code for password

--- userTest.js
+++ userTest.js
@@ -1,6 +1,11 @@
-var config = require('./config');
-var mongoose = require('mongoose');
-mongoose.connect( config.database.mongodb );
-var User = require('./appDomain').dataRepository.User;
-var john = new User({ email: "johntalbot1215@gmail.com", password: "HelloWorld", id: "johntalbot" } );
-john.save();
+// var config = require('./config');
+// var mongoose = require('mongoose');
+// mongoose.connect( config.database.mongodb );
+// var User = require('./appDomain').dataRepository.User;
+// var john = new User({ email: "johntalbot1215@gmail.com", password: "HelloWorld", id: "johntalbot" } );
+// john.save();
+
+var User = require('./appDomain/dataRepository').User;
+var user = new User({});
+user.password = user.generateHash('pass');
+console.log(user.validPassword('pass'));
