3884973e0258250232bdba5b41d7bfa820c47a30
tahoang <dhoang519@gmail.com>
2016-09-12 11:24:23 -0400

    ignore test.js

--- test.js
+++ test.js
@@ -5,8 +5,28 @@ mongoose.connect(config.database.mongodb);
 
 var repo = require('./appDomain/dataRepository');
 
-repo.User.find(function(err, users) {
+var User = repo.User;
 
-  console.log(users);
+
+// var newUser = new repo.User({email: 'dhoang519@gmail.com'});
+// newUser.password = newUser.generateHash('321654d');
+// newUser.save();
+
+// repo.User.find(function(err, users) {
+
+//   console.log(users);
+
+// });
+
+User.findOne({email: 'dhoang519@gmail.com'}, function(err, user){
+	// 	user.email = 'dhoang519@gmail.com';
+	// user.save();
+	console.log('And the user is:');
+	console.log(user);
 
 });
+
+
+
+
+
