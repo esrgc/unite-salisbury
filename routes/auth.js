7154c5187256c2980e05ef629663cbdaa08bbcbf
tahoang <dhoang519@gmail.com>
2016-09-12 10:17:07 -0400

    renamed login to auth

---
+++ routes/auth.js
@@ -0,0 +1,33 @@
+/*
+authentication routes go here
+*/
+
+var express = require('express');
+var router = express.Router();
+var passport = require('../appDomain/authentication').passport;
+
+var domain = require('../appDomain');
+
+/* sign in*/
+router.get('/login', function(req, res) {
+  res.render('login', { title: 'Please log in' , message: req.flash('loginMessage') } );
+});
+
+router.post('/login', passport.authenticate('local-login', {
+  successRedirect: '/', // redirect to success page (will be profile page or event map page)
+  failureRedirect: 'login', // redirect back to the login page
+  failureFlash: true // allow flash messages
+}));
+
+/*sign up*/
+router.get('/signup', function(req, res) {
+  res.render('signup', { title: 'Create profile' , message: req.flash('createMessage') } );
+});
+
+router.post('/signup', passport.authenticate('local-signup', {
+  successRedirect: '/', // redirect to success page (will be profile page or event map page)
+  failureRedirect: 'signup', // redirect back to the login page
+  failureFlash: true // allow flash messages
+}));
+
+module.exports = router;
