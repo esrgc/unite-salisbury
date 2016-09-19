b6826cf7e10720fe52a2600d324cdf84cceac364
tahoang <dhoang519@gmail.com>
2016-09-19 12:26:34 -0400

    Changes to profile edit
    @johntalbot1215 couple things here:
    Please do not use the root path in the router unless the page is showing
    index page for that router. In this case, profile/index (or profile).
    Add action to route if there's any. For example, '/Edit', 'delete',
    'changePassword',....ect.
    Also, when copying and paste, please modify the attritbutes carefully
    and accordingly. See the changes for details.

--- routes/profile.js
+++ routes/profile.js
@@ -1,54 +1,56 @@
 var express = require('express');
 var router = express.Router();
 var domain = require('../appDomain');
-var User = domain.dataRepository.User; 
+var User = domain.dataRepository.User;
 
-router.get('/', function( req, res ){
-    var done = function( err, user ){
-        res.render( 'profile', user );
-    }
-  
-    if( !req.user )
-      res.redirect('/');
-    else {
-      User.findOne({ email: req.user.email }, function( err, user ){
-          if( err ){
-              req.flash('profileMessage', 'Could not find your profile');
-              return done( true, null );
-          }
-          if( user ){
-              return done( false, user );
-          }
-      });
-    }
+router.get('/edit', function(req, res) {
+  var done = function(err, user) {
+    res.render('profile', { user: user, rootPath: '../' });
+  };
+
+  if (!req.user)
+    res.redirect('/'); //use isLoggedIn middleware to check for loggin in user
+  else {
+    User.findOne({ email: req.user.email }, function(err, user) {
+      if (err) {
+        req.flash('profileMessage', 'Could not find your profile');
+        return done(true, null);
+      }
+      if (user) {
+        return done(false, user);
+      }
+    });
+  }
 });
 
-router.post('/', function( req, res ){
-    var done = function( err, user ){
-      res.render( 'profile', { user: user,
-                               message: req.flash('profileMessage'), 
-                              err: err  
-                             });
-    }
-
-    var data = req.body;
-    var updateInstructions = { $set: {
-                                        firstName: data.firstName,
-                                        lastName: data.lastName,
-                                        email: data.email
-                                      }
-                              }
-    var updateOptions = { runValidators: true, new: true };
-    User.findOneAndUpdate({ email: req.user.email },updateInstructions, updateOptions, function( err, user ){
-        if( err ){
-          console.log("There was an error updating data");
-          req.flash('profileMessage', 'Error updating your profile' );
-          done( err, req.user );
-        }
-        else{
-          req.flash('profileMessage', "Profile updated successfuly");
-          done( null, user );
-        }
-       });
+router.post('/edit', function(req, res) {
+  var done = function(err, user) {
+    res.render('profile', {
+      user: user,
+      message: req.flash('profileMessage'),
+      err: err,
+      rootPath: '../'
     });
+  }
+
+  var data = req.body;
+  var updateInstructions = {
+    $set: {
+      firstName: data.firstName,
+      lastName: data.lastName,
+      email: data.email
+    }
+  }
+  var updateOptions = { runValidators: true, new: true };
+  User.findOneAndUpdate({ email: req.user.email }, updateInstructions, updateOptions, function(err, user) {
+    if (err) {
+      console.log("There was an error updating data");
+      req.flash('profileMessage', 'Error updating your profile');
+      done(err, req.user);
+    } else {
+      req.flash('profileMessage', "Profile updated successfuly");
+      done(null, user);
+    }
+  });
+});
 module.exports = router;
