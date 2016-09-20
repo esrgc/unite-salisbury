4984a55a644a3c3e8f0389fb4b9694a26866c877
tahoang <dhoang519@gmail.com>
2016-09-20 14:39:06 -0400

    added manage event page

--- routes/event.js
+++ routes/event.js
@@ -6,8 +6,13 @@ Event map page
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET index page. */
 router.get('/', function(req, res) {
+  res.render('index', { title: 'Express' });
+});
+
+/* GET home page. */
+router.get('/map', function(req, res) {
   res.render('map', { title: 'Express' });
 });
 
