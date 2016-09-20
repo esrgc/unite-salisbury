3c0650445900f05553b1e7bfab8ef88510c6aa38
tahoang <dhoang519@gmail.com>
2016-09-20 15:45:05 -0400

    minor changes

--- routes/event.js
+++ routes/event.js
@@ -5,6 +5,20 @@ Event map page
 
 var express = require('express');
 var router = express.Router();
+var domain = require('../appDomain');
+var User = domain.dataRepository.User;
+
+var isLoggedIn = domain.authentication.isLoggedIn;
+
+//middleware makes sure user is logged in before proceeding.
+router.use(isLoggedIn);
+
+//set root path
+router.use(function(req, res, next) {
+  res.locals.rootPath = '../';
+  next();
+});
+
 
 /* GET index page. */
 router.get('/', function(req, res) {
