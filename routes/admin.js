a30777115b007fc70c9515d9f10fabacd05e400a
tahoang <dhoang519@gmail.com>
2016-09-20 14:23:23 -0400

    implementing admin and minor changes in banner

--- routes/admin.js
+++ routes/admin.js
@@ -8,9 +8,14 @@ var authorized = appDomain.authorization;
 router.use(isLoggedIn);
 router.use(authorized.can('access admin'));
 
+router.use(function(req, res, next) {
+  res.locals.rootPath = '../';
+  next();
+});
+
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('admin', { title: 'Administration' });
+  res.render('admin/index', { title: 'Administration' });
 });
 
 module.exports = router;
