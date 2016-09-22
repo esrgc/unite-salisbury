f1de9bda0c267620e9538c0308adff5565bef473
tahoang <dhoang519@gmail.com>
2016-09-22 15:11:33 -0400

    added addUser page

--- helper/index.js
+++ helper/index.js
@@ -1,6 +1,6 @@
 module.exports = {
   hbsHelpers: function(hbs) {
-  	//register all helpers here
+    //register all helpers here
 
     hbs.registerHelper('parseEvent', function(object) {
       console.log("got object", object);
@@ -9,5 +9,17 @@ module.exports = {
         "<dd> " + object.date.getFullYear() + "</dd>";
     });
     return hbs;
-  };
+  },
+  copy: function(dest, source) {
+    dest = dest || {};
+    if (source) {
+      for (var property in source) {
+        var value = source[property];
+        if (value !== undefined) {
+          dest[property] = value;
+        }
+      }     
+    }
+    return dest;
+  }
 };
--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var appDomain = require('../appDomain');
+var User = appDomain.dataRepository.User;
+
+var helpers = require('../helper');
+
 //is admin middleware (already checks for authentication)
 var isLoggedIn = appDomain.authentication.isLoggedIn;
 var authorized = appDomain.authorization;
@@ -13,19 +17,115 @@ router.use(function(req, res, next) {
   next();
 });
 
-/* GET home page. */
+/* GET admin home page. */
 router.get('/', function(req, res) {
-  res.render('admin/index', { title: 'Administration' });
+  res.render('admin/index', { title: 'Administration', message: req.flash('flashMessage')});
 });
-
-router.get('/manage', function(req, res) {
-  res.render('admin/manage', { title: 'Administration' });
-});
+//add user route
 router.get('/addUser', function(req, res) {
   res.render('admin/addUser', { title: 'Administration' });
 });
+//add user post
+router.post('/addUser', function(req, res, next) {
+  //create a new user
+  var data = req.body;
+  //bind post data to model instance (document)
+  var newUser = new User({
+    email: data.email,
+    password: data.password,
+    firstName: data.firstName,
+    lastName: data.lastName,
+    role: 'user' //default
+  });
+  console.log(data.email);
+  //Now look for email existing
+  User.findOne({ email: data.email }, function(err, user) {
+    if (err) {
+      //error occur
+      req.flash('addUserMessage', 'An error has occured while processing. Please try again!');
+      req.result = {
+        err: true,
+        newUser: null
+      };
+      return next();
+    }
+    if (user) {
+      req.flash('addUserMessage', "That email is already taken. Please try again!");
+      req.result = {
+        err: true,
+        newUser: newUser
+      };
+      return next();
+    } else {
+      //check if password == confirmPass
+      if (data.confirmPassword != data.password) {
+        req.flash('addUserMessage', "Confirmation password does not match. Please try again!");
+        req.result = {
+          err: true,
+          newUser: newUser
+        };
+        return next();
+        //return done(true, newUser);
+      }
+      //now validate the model
+      var validateErr = newUser.validateSync();
+      //validation errors occur
+      if (typeof validateErr != 'undefined') {
+        console.log(validateErr);
+        req.flash('addUserMessage', "Data entered is invalid. Please try again!");
+        req.result = {
+          err: true,
+          newUser: newUser,
+          valErr: validateErr
+        };
+        return next();
+      }
+      //no validation error -> now hash the password
+      newUser.password = newUser.generateHash(newUser.password);
+
+      //everything is good now save the user
+      newUser.save(function(err) { // save
+        if (err) {
+          req.flash('addUserMessage', "Error saving data to the database. Please try again!");
+          req.result = {
+            err: true,
+            newUser: newUser
+          };
+        } else {
+          
+          req.result = {
+            err: false,
+            newUser: newUser
+          };
+        }
+        next();
+      }); //End newUser.save
+    } //End else
+  }); //End find one
+}, function(req, res) { //sub-stacked middleware
+  var result = req.result;
+  result = helpers.copy(result, {
+    title: 'Add User',
+    message: req.flash('addUserMessage')
+  });
+  //if error render the form again
+  if (result.err) {
+    res.render('admin/addUser', result);
+  } else {
+  	req.flash('flashMessage', "User has been added successfully!");
+    //redirect to addmin page   
+    res.redirect('/admin');
+  }
+});
+
+
+router.get('/manageUser', function(req, res) {
+  res.render('admin/manageUser', { title: 'Administration' });
+});
+
 
 router.get('/manageEvent', function(req, res) {
   res.render('admin/manageEvent', { title: 'Administration' });
 });
+
 module.exports = router;
