18ddc7f44b4f81079ec224b3e7808c42094c3c05
tahoang <dhoang519@gmail.com>
2016-09-29 16:26:14 -0400

    pagination implemented

--- helper/index.js
+++ helper/index.js
@@ -41,21 +41,85 @@ module.exports = {
       return html;
     });
 
-    hbs.registerHelper('generatePageUrl', function(data, baseUrl) {
+    hbs.registerHelper('generatePagination', function(data, baseUrl) {
       var url = baseUrl + '?' + [
-        'pageSize=' + data.pageSize,        
-        'sortBy=' + sortBy,
-        'order=' + data.order, 
+        'pageSize=' + data.pageSize,
+        'sortBy=' + data.sortBy,
+        'order=' + data.order,
         'search=' + data.search,
         'searchBy=' + data.searchBy
       ].join('&');
 
+
+      var html = [
+        '<small> Showing ' + data.page + ' of ' + data.pageCount + '</small>',
+        '<nav aria-label="Page navigation">',
+        '<ul class="pagination">'
+      ];
+
+      var firstBtn = [
+        '<li class="' + (data.page != 1 ? '' : 'disabled') + '"">',
+        '<a href="' + url + '&page=1" aria-label="Previous">',
+        '<span aria-hidden="true">&laquo;&laquo;</span>',
+        '</a>',
+        '</li>'
+      ];
+      html = html.concat(firstBtn);
+      //generate previous link btn
+      var prevBtn = [
+        '<li class="' + (data.page > 1 ? '' : 'disabled') + '"">',
+        '<a href="' + url + '&page=' + (data.page - 1) + '" aria-label="Previous">',
+        '<span aria-hidden="true">&laquo;</span>',
+        '</a>',
+        '</li>'
+      ];
+      //merge to html
+      html = html.concat(prevBtn);
+
+
+      //generate paging buttons
+      for (var i = data.page - 2; i < data.page + 3; i++) {
+        if (i > 0 && i <= data.pageCount) {
+          html = html.concat([
+            '<li class="' + (i != data.page ? '' : 'disabled') + '"">',
+            '<a href="' + url + '&page=' + i + '" aria-label="page '+ i +'">',
+            '<span aria-hidden="true">'+ i +'</span>',
+            '</a>',
+            '</li>'
+          ]);
+        }
+      }
+      //generate next btn
+      var nextBtn = [
+        '<li class="' + (data.pageCount > data.page ? '' : 'disabled') + '"">',
+        '<a href="' + url + '&page=' + (data.page - 1) + '" aria-label="Next">',
+        '<span aria-hidden="true">&raquo;</span>',
+        '</a>',
+        '</li>'
+      ];
+       //merge to html
+      html = html.concat(nextBtn);
+      //last btn
+      var lastBtn = [
+        '<li class="' + (data.page != data.pageCount ? '' : 'disabled') + '"">',
+        '<a href="' + url + '&page=1" aria-label="Previous">',
+        '<span aria-hidden="true">&raquo;&raquo;</span>',
+        '</a>',
+        '</li>'
+      ];
+      html = html.concat(lastBtn);
      
-      return html;
+      //closing tags
+      html = html.concat([
+        '</ul>',
+        '</nav>'
+      ]);
+      // console.log(html.join(''));
+      return html.join('');
     });
 
     return hbs;
-  },   
+  },
   copy: function(dest, source) {
     dest = dest || {};
     if (source) {
