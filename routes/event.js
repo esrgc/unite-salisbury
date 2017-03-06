61dcc92ec6970f9e1c2a7072587ca4c81d3cc0f1
Tu Hoang <dhoang519@gmail.com>
2017-03-06 15:26:52 -0500

    added event duplicate check

--- routes/event.js
+++ routes/event.js
@@ -45,119 +45,134 @@ router.get('/add', function(req, res) {
 //post for add event
 router.post('/add', function(req, res) {
   var model = req.body;
-
+  console.log(model);
   var newEvent = new Event();
   //copy the model properties
   newEvent = Object.assign(newEvent, model);
 
+  //check for existing event
+  var promise = Event.findOne({ name: model.name });
 
+  promise.then(function(data) {
+    console.log('Checking for existing event...');
+    console.log(data);
+    if (data != null)
+      return res.render('event/add', {
+        message: 'Event name entered has already been in use. Please use another name!',
+        err: true,
+        event: model
+      });
+    else {
 
+      //parse recurring event to generate schedule
+      if (newEvent.repeat) {
+        let frequency = newEvent.frequency;
+        switch (frequency) {
+          case 'daily':
+            break;
+          case 'weekly':
+            if (typeof model.weeklyDayOfWeek == 'undefined')
+              return res.render('event/add', {
+                message: 'Please specify day of week for weekly recurring!',
+                err: true,
+                event: model
+              });
+            newEvent.dayOfWeek = model.weeklyDayOfWeek;
 
-  //parse recurring event to generate schedule
-  if (newEvent.repeat) {
-    let frequency = newEvent.frequency;
-    switch (frequency) {
-      case 'daily':
-        break;
-      case 'weekly':
-        if (typeof model.weeklyDayOfWeek == 'undefined')
-          return res.render('event/add', {
-            message: 'Please specify day of week for weekly recurring!',
-            err: true,
+            break;
+          case 'monthly':
+            //day of week
+            if (model.monthlyOnType == 'dayOfWeek') {
+              newEvent.dayOfWeek = model.monthlyDayOfWeek;
+              newEvent.dayOfWeekCount = model.monthlyDayOfWeekCount;
+              console.log('day of week count ' + newEvent.dayOfWeekCount);
+            }
+            //day of month
+            if (model.monthlyOnType == 'dayOfMonth') {
+              newEvent.dayOfMonth = model.monthlyDayOfMonth;
+            }
+            break;
+          case 'yearly':
+            newEvent.monthOfYear = model.monthOfYear;
+            if (model.yearlyDayOfWeekMode == 'true') {
+              newEvent.dayOfWeekCount = model.yearlyDayOfWeekCount;
+              newEvent.dayOfWeek = model.yearlyDayOfWeek;
+            }
+            break;
+        }
+      }
+      // console.log(model);
+
+      newEvent.validate((err) => {
+        if (err) {
+          //do a flash message here
+          res.render('event/add', {
+            message: 'Error creating new event. Please try again!',
+            err: err,
             event: model
           });
-        newEvent.dayOfWeek = model.weeklyDayOfWeek;
+        } else {
+          //calculate and save model the event recurrence      
+          let occurrences = newEvent.calculateOccurences();
+          res.render('event/add', {
+            message: 'Validated successfully',
+            err: null,
+            event: model
+          });
 
-        break;
-      case 'monthly':
-        //day of week
-        if (model.monthlyOnType == 'dayOfWeek') {
-          newEvent.dayOfWeek = model.monthlyDayOfWeek;
-          newEvent.dayOfWeekCount = model.monthlyDayOfWeekCount;
-          console.log('day of week count ' + newEvent.dayOfWeekCount);
-        }
-        //day of month
-        if (model.monthlyOnType == 'dayOfMonth') {
-          newEvent.dayOfMonth = model.monthlyDayOfMonth;
-        }
-        break;
-      case 'yearly':
-        newEvent.monthOfYear = model.monthOfYear;
-        if (model.yearlyDayOfWeekMode == 'true') {
-          newEvent.dayOfWeekCount = model.yearlyDayOfWeekCount;
-          newEvent.dayOfWeek = model.yearlyDayOfWeek;
-        }
-        break;
-    }
-  }
-  console.log(model);
+          //now geocode
+          geoCoder.search({ //Use geocoder to lookup
+            Street: model.aAddress,
+            City: model.city,
+            State: model.state,
+            ZIP: model.zip
+          }, function(err, res) {
+            if (err) {
+              newEvent.location = null;
+              return;
+            }
+            if (res.candidates.length == 0) { //If no candidates
+              req.flash('eventsMessage', 'Could not find that address, please try again.');
+              newEvent.location = null;
+              return;
+            }
+            for (var i in res.candidates) {
+              var place = res.candidates[i];
+              if (place.score > 79) {
+                let location = place.location; //Else select first candidate
+                newEvent.location = location;
+                break;
+              }
+            }
 
-  newEvent.validate((err) => {
-    if (err) {
-      //do a flash message here
-      res.render('event/add', {
-        message: 'Error creating new event. Please try again!',
-        err: err,
-        event: model
-      });
-    } else {
-      //calculate and save model the event recurrence      
-      let occurrences = newEvent.calculateOccurences();
-      res.render('event/add', {
-        message: 'Validated successfully',
-        err: null,
-        event: model
-      });
+            // console.log(occurrences);
+            console.log('After geocoding...')
+            console.log(newEvent);
 
-      //now geocode
-      geoCoder.search({ //Use geocoder to lookup
-        Street: model.aAddress,
-        City: model.city,
-        State: model.state,
-        ZIP: model.zip
-      }, function(err, res) {
-        if (err){
-        	newEvent.location = null;
+          });
+
+
           return;
-        }
-        if (res.candidates.length == 0) { //If no candidates
-          req.flash('eventsMessage', 'Could not find that address, please try again.');
-          newEvent.location = null;
-          return;
-        }
-        for (var i in res.candidates) {
-          var place = res.candidates[i];
-          if (place.score > 79) {
-            let location = place.location; //Else select first candidate
-            newEvent.location = location;
-            break;
-          }
-        }
 
-        // console.log(occurrences);
-        console.log('After geocoding...')
-        console.log(newEvent);
-
+          // if (newEvent) {
+          // newEvent.save((err) => {
+          //   if (err)
+          //     res.render('event/add', {
+          //       event: newEvent,
+          //       message: 'Error saving event...Please try again!'
+          //     }); //do a flash message and redisplay
+          //   else {
+          //     req.flash('message', 'Event added successfully!');
+          //     res.redirect('/'); //redirect to index page;          
+          //   }
+          // });
+          // }
+        }
       });
-
-
-      return;
-
-      // if (newEvent) {
-      // newEvent.save((err) => {
-      //   if (err)
-      //     res.render('event/add', {
-      //       event: newEvent,
-      //       message: 'Error saving event...Please try again!'
-      //     }); //do a flash message and redisplay
-      //   else {
-      //     req.flash('message', 'Event added successfully!');
-      //     res.redirect('/'); //redirect to index page;          
-      //   }
-      // });
-      // }
     }
-  });
+  }, function(err) {});
+
+
 
 
 });
