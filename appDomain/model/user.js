cafb30f7598c32133d60eddf958dd00d831edf1b
tahoang <dhoang519@gmail.com>
2017-10-17 10:58:53 -0400

    added property "approved"

--- appDomain/model/user.js
+++ appDomain/model/user.js
@@ -18,9 +18,9 @@ var passwordCaps = function(password) {
 }
 
 var passwordNum = function(password) {
-    return /[0-9]/.test(password);
+  return /[0-9]/.test(password);
 }
-  //Multiple validators with different error messages
+//Multiple validators with different error messages
 var passwordValidators = [
   { validator: passwordLength, message: 'Password must be at least 8 characters long' },
   { validator: passwordCaps, message: 'Password must contain an uppercase character' },
@@ -28,10 +28,10 @@ var passwordValidators = [
 ]
 
 var emailValidate = function(email) {
-  if (email.length == 0) 
+  if (email.length == 0)
     return false;
   return /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/.test(email);
-  
+
 }
 
 
@@ -56,9 +56,10 @@ var UserSchema = new Schema({
   firstName: { type: String, required: [true, 'First name is required'] },
   lastName: { type: String, required: [true, 'Last name is required'] },
   role: String,
+  approved: { type: String, default: false },
   events: [{ type: Schema.Types.ObjectId, ref: 'Event' }] //populated fields
 }, {
-  validateBeforeSave: false//prevent pre-save validation
+  validateBeforeSave: false //prevent pre-save validation
 });
 
 
@@ -66,7 +67,7 @@ var UserSchema = new Schema({
 //
 //
 UserSchema.methods.validPassword = function(password) {
-   return bcrypt.compareSync(password, this.password);
+  return bcrypt.compareSync(password, this.password);
 };
 
 // generating a hash
@@ -75,8 +76,8 @@ UserSchema.methods.generateHash = function(password) {
 };
 
 //to add an event reference
-UserSchema.methods.pushEvent = function( eventID ){
-  this.events.push( eventID );
+UserSchema.methods.pushEvent = function(eventID) {
+  this.events.push(eventID);
 }
 
 module.exports = mongoose.model('User', UserSchema);
