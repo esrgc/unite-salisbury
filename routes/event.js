df687a36aff0aa233e197b84e45bec7e926784c3
tahoang <dhoang519@gmail.com>
2017-10-31 17:39:59 -0400

    fixed bug with id when re-displaying data on error

--- routes/event.js
+++ routes/event.js
@@ -389,190 +389,196 @@ router.get('/edit', isLoggedIn, authorized.can('manage event'), function(req, re
   let p = Event.findOne({ _id: id });
 
   p.then((data) => {
-    let d = data.toObject();
-    //got data 
-    switch (d.frequency) {
-      case 'weekly':
-        d.weeklyDayOfWeek = d.dayOfWeek;
-        break;
-      case 'monthly':
-        if (d.dayOfWeekCount != null)
-          d.monthlyOnType = 'dayOfWeek';
-        else
-          d.monthlyOnType = 'dayOfMonth';
+      let d = data.toObject();
+      //got data 
+      switch (d.frequency) {
+        case 'weekly':
+          d.weeklyDayOfWeek = d.dayOfWeek;
+          break;
+        case 'monthly':
+          if (d.dayOfWeekCount != null)
+            d.monthlyOnType = 'dayOfWeek';
+          else
+            d.monthlyOnType = 'dayOfMonth';
 
-        d.monthlyDayOfMonth = d.dayOfMonth;
-        d.monthlyDayOfWeekCount = d.dayOfWeekCount;
-        d.monthlyDayOfWeek = d.dayOfWeek;
-        break;
-      case 'yearly':
-        d.monthOfYear = d.monthOfYear;
-        d.yearlyDayOfWeekMode = (d.dayOfWeekCount != null).toString();
-        d.yearlyDayOfWeekCount = d.dayOfWeekCount;
-        d.yearlyDayOfWeek = d.dayOfWeek;
-        break
-    };
-    console.log(d);
-    res.render('event/edit', {
-      event: d
+          d.monthlyDayOfMonth = d.dayOfMonth;
+          d.monthlyDayOfWeekCount = d.dayOfWeekCount;
+          d.monthlyDayOfWeek = d.dayOfWeek;
+          break;
+        case 'yearly':
+          d.monthOfYear = d.monthOfYear;
+          d.yearlyDayOfWeekMode = (d.dayOfWeekCount != null).toString();
+          d.yearlyDayOfWeekCount = d.dayOfWeekCount;
+          d.yearlyDayOfWeek = d.dayOfWeek;
+          break
+      };
+      console.log(d);
+      res.render('event/edit', {
+        event: d
+      });
+    })
+    .catch((err) => {
+      //error occurs
+      return req.flash('message', 'Error reading from database...');
+      res.redirect('manage');
     });
-  }, (err) => {
-    //error occurs
-    req.flash('message', 'Error reading from database...');
-    res.redirect('manage');
-  });
 });
 //edit event route (POST)
 router.post('/edit', isLoggedIn, authorized.can('manage event'), function(req, res) {
   var model = req.body;
   let id = model.id;
+  model._id = id;//copy for re-display on error
+  console.log('..........Editing Event.............');
   console.log(model);
   let promise = Event.findOne({ _id: id });
 
   //copy the model properties
-  promise.then(function(data) {
-    let editingEvent = data;
+  promise.then((data) => {
+      let editingEvent = data;
 
-    console.log('Checking for existing event...');
-    editingEvent = Object.assign(editingEvent, model);
+      console.log('found the existing event...');
+      editingEvent = Object.assign(editingEvent, model);
 
 
-    console.log('No existing event...proceeding to create a new one.');
-    //parse recurring event to generate schedule
-    if (editingEvent.repeat) {
-      let frequency = editingEvent.frequency;
-      switch (frequency) {
-        case 'daily':
-          break;
-        case 'weekly':
-          if (typeof model.weeklyDayOfWeek == 'undefined')
-            return res.render('event/edit', {
-              message: 'Please specify day of week for weekly recurring!',
-              err: true,
-              event: model
-            });
-          editingEvent.dayOfWeek = model.weeklyDayOfWeek;
-
-          break;
-        case 'monthly':
-          //day of week
-          if (model.monthlyOnType == 'dayOfWeek') {
-            editingEvent.dayOfWeek = model.monthlyDayOfWeek;
-            editingEvent.dayOfWeekCount = model.monthlyDayOfWeekCount;
-            console.log('day of week count ' + editingEvent.dayOfWeekCount);
-          }
-          //day of month
-          if (model.monthlyOnType == 'dayOfMonth') {
-            editingEvent.dayOfMonth = model.monthlyDayOfMonth;
-          }
-          break;
-        case 'yearly':
-          editingEvent.monthOfYear = model.monthOfYear;
-          if (model.yearlyDayOfWeekMode == 'true') {
-            editingEvent.dayOfWeekCount = model.yearlyDayOfWeekCount;
-            editingEvent.dayOfWeek = model.yearlyDayOfWeek;
-          }
-          break;
-      }
-    }
-    // console.log(model);
-    //validate event
-    editingEvent.validate((err) => {
-      if (err) {
-        //do a flash message here
-        return res.render('event/edit', {
-          message: 'Error creating new event. Please try again!',
-          err: err,
-          event: model
-        });
-      } else {
-        //calculate and save model the event recurrence      
-        editingEvent.calculateSchedule();
-
-        //now geocode
-        geoCoder.search({ //Use geocoder to lookup
-          Street: model.address.replace('.', ''),
-          City: model.city,
-          State: model.state,
-          ZIP: model.zip
-        }, function(err, response) {
-          console.log(response);
-          if (err) {
-            editingEvent.location = null;
-            // return;
-            return res.render('event/edit', {
-              message: 'Error locating the address. Please try another address!',
-              err: {},
-              event: model
-            });
-          } else {
-            if (response.candidates.length == 0) { //If no candidates
-              // req.flash('eventsMessage', 'Could not find that address, please try again.');
+      console.log('Updating event.......');
+      //parse recurring event to generate schedule
+      if (editingEvent.repeat) {
+        let frequency = editingEvent.frequency;
+        switch (frequency) {
+          case 'daily':
+            break;
+          case 'weekly':
+            if (typeof model.weeklyDayOfWeek == 'undefined')
               return res.render('event/edit', {
-                message: 'Could not locate the address. Please try another address!',
+                message: 'Please specify day of week for weekly recurring!',
+                err: true,
+                event: model
+              });
+            editingEvent.dayOfWeek = model.weeklyDayOfWeek;
+
+            break;
+          case 'monthly':
+            //day of week
+            if (model.monthlyOnType == 'dayOfWeek') {
+              editingEvent.dayOfWeek = model.monthlyDayOfWeek;
+              editingEvent.dayOfWeekCount = model.monthlyDayOfWeekCount;
+              console.log('day of week count ' + editingEvent.dayOfWeekCount);
+            }
+            //day of month
+            if (model.monthlyOnType == 'dayOfMonth') {
+              editingEvent.dayOfMonth = model.monthlyDayOfMonth;
+            }
+            break;
+          case 'yearly':
+            editingEvent.monthOfYear = model.monthOfYear;
+            if (model.yearlyDayOfWeekMode == 'true') {
+              editingEvent.dayOfWeekCount = model.yearlyDayOfWeekCount;
+              editingEvent.dayOfWeek = model.yearlyDayOfWeek;
+            }
+            break;
+        }
+      }
+      // console.log(model);
+      //validate event
+      editingEvent.validate((err) => {
+        if (err) {
+          //do a flash message here
+          return res.render('event/edit', {
+            message: 'Error creating new event. Please try again!',
+            err: err,
+            event: model
+          });
+        } else { //calculate and save model the event recurrence      
+          editingEvent.calculateSchedule();
+
+          //now geocode
+          geoCoder.search({ //Use geocoder to lookup
+            Street: model.address.replace('.', ''),
+            City: model.city,
+            State: model.state,
+            ZIP: model.zip
+          }, function(err, response) {
+            console.log(response);
+            if (err) {
+              editingEvent.location = null;
+              // return;
+              return res.render('event/edit', {
+                message: 'Error locating the address. Please try another address!',
                 err: {},
                 event: model
               });
-              editingEvent.location = null;
-              // return;
             } else {
-              for (var i in response.candidates) {
-                var place = response.candidates[i];
-                if (place.score > 79) {
-                  let location = place.location; //Else select first candidate
-                  editingEvent.location = location;
-                  break;
+              if (response.candidates.length == 0) { //If no candidates
+                // req.flash('eventsMessage', 'Could not find that address, please try again.');
+                console.log('No address found...returning to client!');
+                console.log(model);
+                return res.render('event/edit', {
+                  message: 'Could not locate the address. Please try another address!',
+                  err: {},
+                  event: model
+                });
+                editingEvent.location = null;
+                // return;
+              } else {
+                for (var i in response.candidates) {
+                  var place = response.candidates[i];
+                  if (place.score > 79) {
+                    let location = place.location; //Else select first candidate
+                    editingEvent.location = location;
+                    break;
+                  }
                 }
               }
             }
-          }
 
 
-          // console.log(occurrences);
-          console.log('After geocoding...')
-          console.log(editingEvent);
-          //saving owner ref
-          //editingEvent._creator = req.user._id;
+            // console.log(occurrences);
+            console.log('After geocoding...')
+            console.log(editingEvent);
+            //saving owner ref
+            //editingEvent._creator = req.user._id;
 
-          // res.render('event/edit',{
-          //   message: 'validated successfully! can be added',
-          //   event: model
+            // res.render('event/edit',{
+            //   message: 'validated successfully! can be added',
+            //   event: model
+            // });
+            //save event and return
+            let p = editingEvent.save();
+
+            p.then(function(data) {
+              req.flash('message', `Event ${data.name} was edited successfully!`)
+              return res.redirect('manage');
+            }, function(error) {
+              return res.render('event/edit', {
+                message: 'Error saving event! Please try again!',
+                err: error,
+                event: model
+              });
+            })
+
+          });        
+          // if (newEvent) {
+          // newEvent.save((err) => {
+          //   if (err)
+          //     res.render('event/add', {
+          //       event: newEvent,
+          //       message: 'Error saving event...Please try again!'
+          //     }); //do a flash message and redisplay
+          //   else {
+          //     req.flash('message', 'Event added successfully!');
+          //     res.redirect('/'); //redirect to index page;          
+          //   }
           // });
-          //save event and return
-          let p = editingEvent.save();
-
-          p.then(function(data) {
-            req.flash('message', `Event ${data.name} was edited successfully!`)
-            return res.redirect('manage');
-          }, function(error) {
-            return res.render('event/edit', {
-              message: 'Error saving event! Please try again!',
-              err: error,
-              event: model
-            });
-          })
-
-        });
-
-
-        return;
-
-        // if (newEvent) {
-        // newEvent.save((err) => {
-        //   if (err)
-        //     res.render('event/add', {
-        //       event: newEvent,
-        //       message: 'Error saving event...Please try again!'
-        //     }); //do a flash message and redisplay
-        //   else {
-        //     req.flash('message', 'Event added successfully!');
-        //     res.redirect('/'); //redirect to index page;          
-        //   }
-        // });
-        // }
-      }
+          // }
+        }
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      //error occurs
+      req.flash('message', 'Error reading from database...');
+      return res.redirect('manage');
     });
-  });
 
 
 
